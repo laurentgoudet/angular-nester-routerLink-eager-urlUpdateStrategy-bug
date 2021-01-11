99d026caee1ced3a6933a46b8d1bb4e529595bcc
Laurent Goudet <me@laurentgoudet.com>
2021-01-11 10:29:56 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    Hello
+    <h1>Angular Router App</h1>
+    <!-- This nav gives you links to click, which tells the router which route to use (defined in the routes constant in  AppRoutingModule) -->
+    <nav>
+      <ul>
+        <li><a routerLink="/first-component" routerLinkActive="active">First Component</a></li>
+        <li><a routerLink="/second-component" routerLinkActive="active">Second Component</a></li>
+      </ul>
+    </nav>
+    <div *ngPf="foo"></div>
+    <div ngPf="foo"></div>
+    <ng-container *ngTemplateOutle="myTemplate"></ng-container>
+    <ng-template #myTemplate>
+      My template is here
+    </ng-template>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
   title = 'test-ng10';
