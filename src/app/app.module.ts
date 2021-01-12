53c3bf06b1122880c06947ef0a1ea4189bf268c9
Laurent Goudet <me@laurentgoudet.com>
2021-01-12 15:22:21 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { LinkWrapperComponent } from './link-wrapper/link-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
-    SecondComponent,
-    LinkWrapperComponent
   ],
   imports: [
     BrowserModule,
